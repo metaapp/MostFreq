5d12203ad470d5e0b5ec0a0d86256bd74cae4774
Kubo <mazgut@gmail.com>
2016-07-06 18:51:12 +0200

    Final source code

--- kmeans.cs
+++ kmeans.cs
@@ -9,34 +9,79 @@ namespace CeAI
 {
     class Point
     {
-        double x;
-        double y;
+        public double x;
+        public double y;
+        public int center;
+        public int numPointsInCluster;
 
         public Point(double px, double py)
         {
             x = px;
             y = py;
+            center = -1;
         }
     }
 
-    static class KMeans
+    class KMeans
     {
+        private List<Point> _data = null;
 
-
-        public static void LoadData()
+        public void LoadData()
         {
             char[] delimiterXY = new char[] { ' ' };
             char[] delimieterLine = new char[] { '\n' };
             string[] data = File.ReadAllText(@"c:\Users\Kubo\Documents\data.txt").Split(delimieterLine);
-            List<Point> points = new List<Point>(data.Count());
+            _data = new List<Point>(data.Count());
             
-
             foreach (string line in data)
             {
                 string[] stringCoords = line.Split(delimiterXY);
                 if(stringCoords.Count() == 2)
-                    points.Add(new Point(double.Parse(stringCoords[0], System.Globalization.CultureInfo.InvariantCulture), double.Parse(stringCoords[1], System.Globalization.CultureInfo.InvariantCulture)));
+                    _data.Add(new Point(double.Parse(stringCoords[0], System.Globalization.CultureInfo.InvariantCulture), double.Parse(stringCoords[1], System.Globalization.CultureInfo.InvariantCulture)));
             }
+        }
+
+        public List<Point> Run(int numOfClusters, int steps)
+        {
+            List<Point> centers = new List<Point>(numOfClusters);
+            List<Point> newCenters = new List<Point>(numOfClusters); ;
+
+            // todo: random init, now: first points
+            for (int k = 0; k < numOfClusters; k++)
+                centers.Add(_data[k]);
+
+            for (int s = 0; s < steps; s++)
+            {
+                newCenters.Clear();
+                for (int k = 0; k < numOfClusters; k++)
+                    newCenters.Add(new Point(0, 0));
+
+                for (int p = 0; p < _data.Count; p++)
+                {
+                    double prevDistance = double.MaxValue;
+                    for (int k = 0; k < numOfClusters; k++)
+                    {
+                        double distance = Math.Pow(_data[p].x - centers[k].x, 2) + Math.Pow(_data[p].y - centers[k].y, 2);
+                        if (distance < prevDistance)
+                        {
+                            _data[p].center = k;
+                            prevDistance = distance;
+                        }
+                    }
+
+                    newCenters[_data[p].center].x += _data[p].x;
+                    newCenters[_data[p].center].y += _data[p].y;
+
+                    // according to cluster, increment counter
+                }
+
+                // divide cluster positions by counts
+
+                // set new positions of centers
+
+                // if positions of centers dont change then break
+            }
+
         }
     }
 }
